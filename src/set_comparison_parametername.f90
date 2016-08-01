d5b78e385680fdf84c4c4a7aa468aa08d89ddf1e
Kazutake Asahi <kazutake.asahi@river-link.co.jp>
2016-08-01 16:24:38 +0900

    csvファイルの読み方をlineで読んで、コンマでsplitする方法に変更

--- src/set_comparison_parametername.f90
+++ src/set_comparison_parametername.f90
@@ -9,6 +9,9 @@
    implicit none
    integer :: i, ii, n, icount, l
    character(len=strmax) :: w, a
+	
+	integer :: ipos1, ipos2,buf_len
+	character(len=strmax) :: buf, row_id
 !----------------------------------------------------------------------
    
 	pname = ""
@@ -18,8 +21,8 @@
    
    icount = 0
    do
-      read (1, *, end=100) ii, w  ! ファイル終端ならば999に飛ぶ
-      icount = icount + 1
+      read (1, *, err=100, end=100) ii, w  ! ファイル終端ならば999に飛ぶ
+		icount = icount + 1
    end do
 100 continue   
    
@@ -29,8 +32,33 @@
    
    rewind (1)           !　ファイルの頭に戻る
    read (1, '()')       !　ヘッダ行の読み飛ばし
+	
    do i = 1, num_p
-      read(1, *, err = 998) ii, (pname(n,i), n=0, nmax-1)
+      !read(1, *, err = 998) ii, (pname(n,i), n=0, nmax-1)
+		
+		read(1, "(a)", err = 998) buf
+		
+		buf_len = len_trim(buf)
+		ipos1 = 1	
+		n=-1
+		do 
+			buf_len = len_trim(buf)
+			ipos2 = scan(buf, ",")
+			if(n>=0)then
+				if(ipos2==0) then
+					ipos2 = buf_len
+					if(buf(ipos1:ipos2-1)=="") exit
+					pname(n,i) = buf(ipos1:ipos2)
+				else
+					if(buf(ipos1:ipos2-1)=="") exit
+					pname(n,i) = buf(ipos1:ipos2-1)
+				end if
+			end if
+			
+			buf = buf(ipos2+1:len(buf))
+			
+			n = n+1
+		end do 
    end do
 	
 	do i= 1, num_p
